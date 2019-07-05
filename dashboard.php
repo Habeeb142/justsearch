5286873effd206af0c4d876a11acb970c2c46161
Mustapha Habeeb <43966404+Habeeb142@users.noreply.github.com>
2019-07-05 20:28:06 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -28,9 +28,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <link rel="stylesheet" href="index.css">
-	<https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css>
-    <https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js>
-	<script type="text/javascript" src="jquery3.3.1.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="jquery3.3.1.js"></script>
 </head>
 <script>
     $(document).ready(function(){
