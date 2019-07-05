1c188e34b8dac8769f71881c97a50de08468da67
Mustapha Habeeb <43966404+Habeeb142@users.noreply.github.com>
2019-07-05 20:33:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
     <title>Home - JustSearch</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css>
-    <https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js>
-    <link rel="stylesheet" href="index.css">
-	
-	<script type="text/javascript" src="jquery3.3.1.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="index.css">
+       <script type="text/javascript" src="jquery3.3.1.js"></script>
+        <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 </head>
 <script>
 
